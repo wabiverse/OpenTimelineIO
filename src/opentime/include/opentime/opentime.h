99aa1bc9cda0c248a78baa7f4fa108c8694eda82
furby™ <devs@wabi.foundation>
2024-02-13 19:46:28 -0600

    Move opentime & opentimelineio to include dirs.

---
+++ src/opentime/include/opentime/opentime.h
@@ -0,0 +1,11 @@
+#ifndef __OPENTIME_H__
+#define __OPENTIME_H__
+
+#include "opentime/version.h"
+#include "opentime/errorStatus.h"
+#include "opentime/timeRange.h"
+#include "opentime/rationalTime.h"
+#include "opentime/timeTransform.h"
+#include "opentime/stringPrintf.h"
+
+#endif // __OPENTIME_H__
